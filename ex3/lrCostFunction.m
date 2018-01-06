71019e55b95598f1c4d5453e1f9bc32fadb47b26
7pao <34097145+7pao@users.noreply.github.com>
2018-01-06 18:02:16 +0800

    assignment

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -35,7 +35,8 @@ grad = zeros(size(theta));
 %           temp(1) = 0;   % because we don't add anything for j = 0  
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
-
+J = 1/m * (-y' * log(sigmoid(X * theta)) - (1 - y)' * log(1 - sigmoid(X * theta))) + lambda/2/m * (theta(2:end)' * theta(2:end));
+grad = 1/m * X' * (sigmoid(X * theta) - y) + lambda/m * [0 ; theta(2:end)];
 
 
 
